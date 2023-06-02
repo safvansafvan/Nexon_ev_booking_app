d77efd7ab88ba735ba08ea0f070cf0d2b8ca7138
safvan <sajusajuptl@gmail.com>
2023-06-02 19:49:16 +0530

    login page c

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -10,102 +10,136 @@ class LoginPage extends StatelessWidget {
     var screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          height: double.infinity,
-          width: double.infinity,
-          color: const Color.fromARGB(255, 241, 239, 239),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 15, top: screenHeight * 0.06),
-                child: Text(
-                  "Welcome Back!!",
-                  style: textStyleFuc(
-                      weight: FontWeight.w900, color: kBlack, size: 25),
-                ),
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 15, top: screenHeight * 0.03),
+              child: Text(
+                "Welcome Back!!",
+                style: textStyleFuc(
+                    weight: FontWeight.w900, color: kBlack, size: 25),
               ),
-              Padding(
-                padding: EdgeInsets.only(
-                    top: screenHeight * 0.03, left: 10, right: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "User Name",
-                        labelText: "Name",
-                        border: OutlineInputBorder(borderRadius: radiusTen),
-                      ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: screenHeight * 0.03, left: 10, right: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "User Name",
+                      labelText: "Name",
+                      border: OutlineInputBorder(borderRadius: radiusTen),
                     ),
-                    SizedBox(
-                      height: screenHeight * 0.05,
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.02,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      labelText: "Password",
+                      border: OutlineInputBorder(borderRadius: radiusTen),
                     ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Password",
-                        labelText: "Password",
-                        border: OutlineInputBorder(borderRadius: radiusTen),
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(7),
+                    child: Text(
+                      "Forgot Password?",
+                      style: textStyleFuc(
+                          weight: FontWeight.w500, color: kBlack, size: 15),
                     ),
-                    const Padding(
-                      padding: EdgeInsets.all(7),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: screenHeight * 0.07,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 241, 239, 239),
+                      borderRadius: radiusTen,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 15),
                       child: Text(
-                        "Forgot Password?",
+                        "Login",
+                        textAlign: TextAlign.center,
+                        style: textStyleFuc(
+                            weight: FontWeight.w500, color: kBlack, size: 15),
                       ),
                     ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: screenHeight * 0.07,
-                      decoration:
-                          BoxDecoration(color: kwhite, borderRadius: radiusTen),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const FaIcon(
-                            FontAwesomeIcons.google,
-                            color: Colors.red,
-                          ),
-                          const SizedBox(
-                            width: 15,
-                          ),
-                          Text(
-                            'Continue With Google',
-                            style: textStyleFuc(
-                              weight: FontWeight.w500,
-                              color: kBlack,
-                              size: 15,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: screenHeight * 0.05,
-                    ),
-                    Container(
-                      height: 183,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: radiusTen,
-                        image: const DecorationImage(
-                          colorFilter: ColorFilter.mode(
-                            Color.fromARGB(255, 241, 239, 239),
-                            BlendMode.darken,
-                          ),
-                          fit: BoxFit.cover,
-                          image: AssetImage("assets/nexon.jpeg"),
+                  ),
+                  
+                  SizedBox(
+                    height: screenHeight * 0.05,
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: radiusTen,
+                      image: const DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                          Color.fromARGB(255, 241, 239, 239),
+                          BlendMode.darken,
                         ),
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/nexon.jpeg"),
                       ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.04,
+                  ),
+                  Container(
+                    height: screenHeight * 0.07,
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 241, 239, 239),
+                        borderRadius: radiusTen),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const FaIcon(
+                          FontAwesomeIcons.google,
+                          color: Colors.red,
+                        ),
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          'Continue With Google',
+                          style: textStyleFuc(
+                            weight: FontWeight.w500,
+                            color: kBlack,
+                            size: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                  SizedBox(
+                    height: screenHeight * 0.02,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Don't have an account? ",
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Text("Register Now",
+                          style: textStyleFuc(
+                              weight: null, color: Colors.blue, size: 14))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
