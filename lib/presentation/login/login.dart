95b88139888ce121f0abd8f0b242186a0884a6bf
safvan <sajusajuptl@gmail.com>
2023-06-04 15:32:13 +0530

    otp ui

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,10 +1,21 @@
+import 'dart:developer';
+
 import 'package:bookingapp/controller/const.dart';
+import 'package:bookingapp/presentation/login/register.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class LoginPage extends StatelessWidget {
+bool isVisible = true;
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
@@ -23,120 +34,161 @@ class LoginPage extends StatelessWidget {
             Padding(
               padding: EdgeInsets.only(
                   top: screenHeight * 0.03, left: 10, right: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "User Name",
-                      labelText: "Name",
-                      border: OutlineInputBorder(borderRadius: radiusTen),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "User Name",
+                        labelText: "Name",
+                        border: OutlineInputBorder(borderRadius: radiusTen),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Required name ";
+                        }
+                        return null;
+                      },
                     ),
-                  ),
-                  SizedBox(
-                    height: screenHeight * 0.02,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      labelText: "Password",
-                      border: OutlineInputBorder(borderRadius: radiusTen),
+                    SizedBox(
+                      height: screenHeight * 0.02,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(7),
-                    child: Text(
-                      "Forgot Password?",
-                      style: textStyleFuc(
-                          weight: FontWeight.w500, color: kBlack, size: 15),
+                    TextFormField(
+                      obscureText: isVisible,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          icon: Icon(isVisible
+                              ? Icons.visibility_outlined
+                              : Icons.visibility_off_outlined),
+                          onPressed: () {
+                            setState(() {
+                              isVisible = !isVisible;
+                            });
+                            log("$isVisible");
+                          },
+                        ),
+                        hintText: "Password",
+                        labelText: "Password",
+                        helperText: "Password lenght should have >=8 ",
+                        border: OutlineInputBorder(borderRadius: radiusTen),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Required password ";
+                        }
+                        return null;
+                      },
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    height: screenHeight * 0.07,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 241, 239, 239),
-                      borderRadius: radiusTen,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 15),
+                    Padding(
+                      padding: const EdgeInsets.all(7),
                       child: Text(
-                        "Login",
-                        textAlign: TextAlign.center,
+                        "Forgot Password?",
                         style: textStyleFuc(
-                            weight: FontWeight.w500, color: kBlack, size: 15),
-                      ),
-                    ),
-                  ),
-                  
-                  SizedBox(
-                    height: screenHeight * 0.05,
-                  ),
-                  Container(
-                    height: 200,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: radiusTen,
-                      image: const DecorationImage(
-                        colorFilter: ColorFilter.mode(
-                          Color.fromARGB(255, 241, 239, 239),
-                          BlendMode.darken,
-                        ),
-                        fit: BoxFit.cover,
-                        image: AssetImage("assets/nexon.jpeg"),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: screenHeight * 0.04,
-                  ),
-                  Container(
-                    height: screenHeight * 0.07,
-                    decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 241, 239, 239),
-                        borderRadius: radiusTen),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const FaIcon(
-                          FontAwesomeIcons.google,
-                          color: Colors.red,
-                        ),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Text(
-                          'Continue With Google',
-                          style: textStyleFuc(
                             weight: FontWeight.w500,
-                            color: kBlack,
-                            size: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  
-                  SizedBox(
-                    height: screenHeight * 0.02,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Don't have an account? ",
-                        style: TextStyle(fontWeight: FontWeight.w500),
+                            color: const Color.fromARGB(255, 66, 140, 200),
+                            size: 15),
                       ),
-                      Text("Register Now",
-                          style: textStyleFuc(
-                              weight: null, color: Colors.blue, size: 14))
-                    ],
-                  )
-                ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: screenHeight * 0.07,
+                      color: const Color.fromARGB(255, 241, 239, 239),
+                      child: OutlinedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: radiusTen)),
+                          ),
+                          onPressed: () {
+                            
+                          },
+                          child: Text(
+                            'Login',
+                            style: textStyleFuc(
+                                weight: FontWeight.w500, color: null, size: 16),
+                          )),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.05,
+                    ),
+                    Container(
+                      height: 200,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: radiusTen,
+                        image: const DecorationImage(
+                          colorFilter: ColorFilter.mode(
+                            Color.fromARGB(255, 241, 239, 239),
+                            BlendMode.darken,
+                          ),
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/nexon.jpeg"),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.03,
+                    ),
+                    Container(
+                      height: screenHeight * 0.07,
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 241, 239, 239),
+                          borderRadius: radiusTen),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const FaIcon(
+                            FontAwesomeIcons.google,
+                            color: Colors.red,
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            'Continue With Google',
+                            style: textStyleFuc(
+                              weight: FontWeight.w500,
+                              color: kBlack,
+                              size: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight * 0.02,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const RegisterScreen();
+                            },
+                          ),
+                        );
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't have an account? ",
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text("Register Now",
+                              style: textStyleFuc(
+                                  weight: null, color: Colors.blue, size: 14))
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
---
+++ lib/presentation/login/otp.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../../controller/const.dart';
+
+class OtpScreen extends StatelessWidget {
+  const OtpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(children: [
+          ListTile(
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, top: 50, right: 15),
+            child: SizedBox(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "OTP Verification",
+                    style: textStyleFuc(
+                        weight: FontWeight.w900, color: kBlack, size: 25),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                      "Enter the verification code we just sent on\nemail address"),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Form(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(
+                          height: 68,
+                          width: 64,
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: radiusTen)),
+                            style: Theme.of(context).textTheme.titleLarge,
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(1),
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 68,
+                          width: 64,
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: radiusTen)),
+                            style: Theme.of(context).textTheme.titleLarge,
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(1),
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 68,
+                          width: 64,
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: radiusTen)),
+                            style: Theme.of(context).textTheme.titleLarge,
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(1),
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 68,
+                          width: 64,
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: radiusTen)),
+                            style: Theme.of(context).textTheme.titleLarge,
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(1),
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                const  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: screenHeight * 0.07,
+                    color: const Color.fromARGB(255, 241, 239, 239),
+                    child: OutlinedButton(
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(borderRadius: radiusTen)),
+                        ),
+                        onPressed: () {},
+                        child: Text('Verify',style: textStyleFuc(weight: FontWeight.w500, color: null, size: 16),)),
+                  ),
+                  SizedBox(
+                    height: screenHeight * 0.09,
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: radiusTen,
+                      image: const DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                          Colors.white,
+                          BlendMode.darken,
+                        ),
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/nexonOt.webp"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
