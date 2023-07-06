8f78cbb57a7e339e1f57b3228debc578cc509601
safvan <sajusajuptl@gmail.com>
2023-07-06 11:19:45 +0530

    splash navigate solved

--- lib/controller/providers/splash.dart
+++ lib/controller/providers/splash.dart
@@ -6,17 +6,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SplashProvider with ChangeNotifier {
   navigateHome(ctx) async {
     final pref = await SharedPreferences.getInstance();
-    final isLoggedIn = pref.getBool("isLoggedIn") ?? false;
+    final token = pref.getString("ACCESS_TOKEN") ?? "";
     await Future.delayed(const Duration(seconds: 3), () {});
-    if (isLoggedIn == false) {
+    if (token.isEmpty) {
       Navigator.pushReplacement(
         ctx,
         MaterialPageRoute(builder: (context) {
           return const LoginPage();
         }),
       );
-    }
-    if (isLoggedIn == true) {
+    } else {
       Navigator.pushReplacement(
         ctx,
         MaterialPageRoute(builder: (context) {
@@ -24,6 +23,7 @@ class SplashProvider with ChangeNotifier {
         }),
       );
     }
+
     notifyListeners();
   }
 }
